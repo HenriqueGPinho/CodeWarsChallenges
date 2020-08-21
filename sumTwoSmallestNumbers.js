ff01cd5454c22ae382347763a9dbba46c4203d83
Henrique Pinho <henrique.de.nf@gmail.com>
2020-08-21 14:30:32 -0300

    removed unnecessary parenthesys

--- sumTwoSmallestNumbers.js
+++ sumTwoSmallestNumbers.js
@@ -6,7 +6,7 @@ For example, when an array is passed like [19, 5, 42, 2, 77], the output should
 be 7.*/
 
 function sumTwoSmallestNumbers(numbers) {  
-  numbers.sort((a, b) => (a - b));
+  numbers.sort((a, b) => a - b);
   return numbers[0] + numbers[1];
 }
 
