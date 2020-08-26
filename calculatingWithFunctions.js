58e3b728e440a554ae50ada152a31dcdfcea841d
Henrique Pinho <henrique.de.nf@gmail.com>
2020-08-25 22:23:39 -0300

    fixed all functions with best practices

--- calculatingWithFunctions.js
+++ calculatingWithFunctions.js
@@ -18,140 +18,16 @@ Divison should be integer division. For example, this should return 2, not
 2.666666...:
 eight(dividedBy(three()));*/
 
-/*function zero(operation) {
-  if (operation) {
-    const calc = operation.split(' ');
-    switch (calc[0]) {
-      case '+': return 0 + parseInt(calc[1]);
-      case '-': return 0 - parseInt(calc[1]);
-      case '*': return 0 * parseInt(calc[1]);
-      case '/': return parseInt(0 / parseInt(calc[1]));
-    }
-  } else return '0';
-}
-function one(operation) {
-  if (operation) {
-    const calc = operation.split(' ');
-    switch (calc[0]) {
-      case '+': return 1 + parseInt(calc[1]);
-      case '-': return 1 - parseInt(calc[1]);
-      case '*': return 1 * parseInt(calc[1]);
-      case '/': return parseInt(1 / parseInt(calc[1]));
-    }
-  } else return '1';
-}
-function two(operation) {
-  if (operation) {
-    const calc = operation.split(' ');
-    switch (calc[0]) {
-      case '+': return 2 + parseInt(calc[1]);
-      case '-': return 2 - parseInt(calc[1]);
-      case '*': return 2 * parseInt(calc[1]);
-      case '/': return parseInt(2 / parseInt(calc[1]));
-    }
-  } else return '2';
-}
-function three(operation) {
-  if (operation) {
-    const calc = operation.split(' ');
-    switch (calc[0]) {
-      case '+': return 3 + parseInt(calc[1]);
-      case '-': return 3 - parseInt(calc[1]);
-      case '*': return 3 * parseInt(calc[1]);
-      case '/': return parseInt(3 / parseInt(calc[1]));
-    }
-  } else return '3';
-}
-function four(operation) {
-  if (operation) {
-    const calc = operation.split(' ');
-    switch (calc[0]) {
-      case '+': return 4 + parseInt(calc[1]);
-      case '-': return 4 - parseInt(calc[1]);
-      case '*': return 4 * parseInt(calc[1]);
-      case '/': return parseInt(4 / parseInt(calc[1]));
-    }
-  } else return '4';
-}
-function five(operation) {
-  if (operation) {
-    const calc = operation.split(' ');
-    switch (calc[0]) {
-      case '+': return 5 + parseInt(calc[1]);
-      case '-': return 5 - parseInt(calc[1]);
-      case '*': return 5 * parseInt(calc[1]);
-      case '/': return parseInt(5 / parseInt(calc[1]));
-    }
-  } else return '5';
-}
-function six(operation) {
-  if (operation) {
-    const calc = operation.split(' ');
-    switch (calc[0]) {
-      case '+': return 6 + parseInt(calc[1]);
-      case '-': return 6 - parseInt(calc[1]);
-      case '*': return 6 * parseInt(calc[1]);
-      case '/': return parseInt(6 / parseInt(calc[1]));
-    }
-  } else return '6';
-}
-function seven(operation) {
-  if (operation) {
-    const calc = operation.split(' ');
-    switch (calc[0]) {
-      case '+': return 7 + parseInt(calc[1]);
-      case '-': return 7 - parseInt(calc[1]);
-      case '*': return 7 * parseInt(calc[1]);
-      case '/': return parseInt(7 / parseInt(calc[1]));
-    }
-  } else return '7';
-}
-function eight(operation) {
-  if (operation) {
-    const calc = operation.split(' ');
-    switch (calc[0]) {
-      case '+': return 8 + parseInt(calc[1]);
-      case '-': return 8 - parseInt(calc[1]);
-      case '*': return 8 * parseInt(calc[1]);
-      case '/': return parseInt(8 / parseInt(calc[1]));
-    }
-  } else return '8';
-}
-function nine(operation) {
-  if (operation) {
-    const calc = operation.split(' ');
-    switch (calc[0]) {
-      case '+': return 9 + parseInt(calc[1]);
-      case '-': return 9 - parseInt(calc[1]);
-      case '*': return 9 * parseInt(calc[1]);
-      case '/': return parseInt(9 / parseInt(calc[1]));
-    }
-  } else return '9';
-}
-
-function plus(number) {
-  return `+ ${number}`;
-}
-function minus(number) {
-  return `- ${number}`;
-}
-function times(number) {
-  return `* ${number}`;
-}
-function dividedBy(number) {
-  return `/ ${number}`;
-}*/
-
-function zero(operation) {}
-function one(operation) {}
-function two(operation) {}
+function zero(operation) { return operation ? operation(0) : 0 }
+function one(operation) { return operation ? operation(1) : 1 }
+function two(operation) { return operation ? operation(2) : 2 }
 function three(operation) { return operation ? operation(3) : 3 }
-function four(operation) {}
-function five(operation) {}
-function six(operation) {}
-function seven(operation) {}
+function four(operation) { return operation ? operation(4) : 4 }
+function five(operation) { return operation ? operation(5) : 5 }
+function six(operation) { return operation ? operation(6) : 6 }
+function seven(operation) { return operation ? operation(7) : 7 }
 function eight(operation) { return operation ? operation(8) : 8 }
-function nine(operation) {}
+function nine(operation) { return operation ? operation(9) : 9 }
 
 function plus(right) { return left => { return left + right } }
 function minus(right) { return left => { return left - right } }
